cd4256e7f6d1fdbf566389c8628c1d8c0388cf20
sahandmoslemi <sahandmoslemi@gmail.com>
2024-05-07 18:14:35 +0300

    test: Adds test code 4.1.1.

---
+++ test.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
+def test_success_credentials(input):
+    passed = False
+    driver = webdriver.Chrome()
+    identifier, password = input
+
+
+    driver.get("http://127.0.0.1:5000/")
+    time.sleep(3)
+
+    identifier_input = driver.find_element("name", "identifier")
+    password_input = driver.find_element("name", "password")
+
+    identifier_input.send_keys(identifier)
+    password_input.send_keys(password)
+
+    submit_button = driver.find_element("name", "submit")
+    submit_button.click()
+
+    driver.implicitly_wait(10)
+    time.sleep(3)
+
+    if "http://localhost:3000" in driver.current_url:
+        passed = True
+
+    driver.quit()
+
+    return passed
+
+if __name__=="__main__":
+    #4.1.1 Valid Credentials with Phone
+    if test_success_credentials(("+905540244745", "1234$cdA6578")):
+        logger.info("4.1.1 PASSED")
+
+    else:
+        logger.error("4.1.1 FAILED")
